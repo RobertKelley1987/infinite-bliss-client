05168cfb77f2f30f03730e00e11ec0db8093bad5
Robert Kelley <robertkelley1987@gmail.com>
2023-09-28 17:17:12 -0400

    Added production url

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const productionURL = '/';
+const productionURL = 'https://infinite-bliss-server.onrender.com';
 
 export const api = axios.create({
     baseURL: productionURL, 
