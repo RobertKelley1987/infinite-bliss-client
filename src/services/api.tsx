fc6a65bd49c49bd2401344a8ba3412431c88a915
Robert Kelley <robertkelley1987@gmail.com>
2024-01-04 13:13:44 -0500

    Changed http to https in server url.

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let baseURL = "/";
 if (process.env.NODE_ENV === "production") {
-  baseURL = "http://ec2-3-80-48-3.compute-1.amazonaws.com:8080";
+  baseURL = "https://ec2-3-80-48-3.compute-1.amazonaws.com";
 }
 
 export const api = axios.create({ baseURL, withCredentials: true });
