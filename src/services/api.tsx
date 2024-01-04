5e2c852f513e86cbee53d21073022d25f9b9ea12
Robert Kelley <robertkelley1987@gmail.com>
2024-01-04 12:59:20 -0500

    Added url to AWS host for express server.

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-let baseURL = '/';
-if(process.env.NODE_ENV === 'production') {
-    baseURL = 'https://infinite-bliss-server.onrender.com';
+let baseURL = "/";
+if (process.env.NODE_ENV === "production") {
+  baseURL = "http://ec2-3-80-48-3.compute-1.amazonaws.com:8080";
 }
 
 export const api = axios.create({ baseURL, withCredentials: true });
